db43dc17b65b966741a5004c4fcd3047a6c0e2ac
DeepthiJanga <47146999+DeepthiJanga@users.noreply.github.com>
2019-02-13 20:43:07 +0530

    Add files via upload

---
+++ arthimatic.py
@@ -0,0 +1,10 @@
+a=input("enter the value a")
+b=input("enter the value b")
+sum=float(a+b)
+sub=float(a-b)
+mul=float(a*b)
+div=float(a/b)
+print "sum", sum
+print "sub", sub
+print "mul", mul
+print "div", div
