6b8594fa2986884927fbd3a1835dadf74b5f6a41
DeepthiJanga <47146999+DeepthiJanga@users.noreply.github.com>
2019-03-09 22:15:28 +0530

    Add files via upload

--- accum.py
+++ accum.py
@@ -1,10 +1,13 @@
-import matplotlib.pyplot as plt
 import numpy as np
-n=int (input("enter the n value"))
-sum=0
-for i in range(1,n):
-          sum=sum+i
-          p=sum
-          print (p)
-          plt.plot(n,p)
-plt.show( )
+n=int(input("enter number number of samples:"))
+x=[]
+for i in range(n):
+	y=int(input("enter samples:"))
+	x=np.append(x,y)
+print("entered samples:",x)
+y=[]
+s=0
+for i in range(n):
+	s=s+x[i]
+	y=np.append(y,s)
+print("accumulator result:",y)
